071dff3f04e0581a161fd533b286c354bb545466
FARHAN-2-BOT-2 <mkjahid65522@gmail.com>
2025-05-12 02:03:12 +0600

    Update info.js

--- scripts/commands/info.js
+++ scripts/commands/info.js
@@ -54,7 +54,7 @@ _____________🅲🅾🅽🆃🅰🅲🆃_____________
 
 💥𝐅𝐀𝐂𝐄𝐁𝐎𝐎𝐊 𝐈𝐃 (❶)💥 : https://www.facebook.com/profile.php?id=61550529403710
 
-💥𝐅𝐀𝐂𝐄𝐁𝐎𝐎𝐊 𝐈𝐃 (❷)💥 : m.me/61550529403710
+💥𝐅𝐀𝐂𝐄𝐁𝐎𝐎𝐊 𝐈𝐃 (❷)💥 : https://www.facebook.com/profile.php?id=100089582321226
 
 ✴️𝐁𝐎𝐓 𝐏𝐑𝐄𝐅𝐈𝐗✴️ : ${global.config.PREFIX}
 
@@ -78,6 +78,6 @@ _____________🅲🅾🅽🆃🅰🅲🆃_____________
 
 💚🌺𝐓𝐇𝐀𝐍𝐊𝐒 𝐅𝐎𝐑 𝐔𝐒𝐈𝐍𝐆🌺💚 ${global.config.BOTNAME} 『😽🖤🌺』`,attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID, () => 
   fs.unlinkSync(__dirname + "/cache/1.png"));  
-    return request(encodeURI(`https://graph.facebook.com/61550121814016/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`)).pipe(
+    return request(encodeURI(`https://www.facebook.com/profile.php?id=61550529403710/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`)).pipe(
 fs.createWriteStream(__dirname+'/cache/1.png')).on('close',() => callback());
  };
