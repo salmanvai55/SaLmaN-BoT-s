7356447a7ca0915bec8077804f3c1c66746f0651
FARHAN-2-BOT-2 <mkjahid65522@gmail.com>
2025-05-11 20:25:43 +0600

    Update egupre.js

--- scripts/commands/egupre.js
+++ scripts/commands/egupre.js
@@ -16,7 +16,7 @@ handleEvent: function({ api, event, client, __GLOBAL }) {
   var { threadID, messageID } = event;
   const content = event.body ? event.body : '';
   const body = content.toLowerCase();
-  if (body.indexOf("🥹🙄🙄🙄")==0 || body.indexOf("🙄🙄")==0 || body.indexOf("🙄🙄")==0 || body.indexOf("🙄")==0) {
+  if (body.indexOf("😒")==0 || body.indexOf("🤔")==0 || body.indexOf("🤨")==0 || body.indexOf("🙄")==0) {
     var msg = {
         body: "উ্ঁপ্রে্ঁ কি্ঁ দে্ঁখো্ঁ🙄🥀",
         attachment: fs.createReadStream(__dirname + `/Nayan/upray.mp3`)
