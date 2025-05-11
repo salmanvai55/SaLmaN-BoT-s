1aee9cd47e5df2e82f6e70ddc659e37054d816e1
FARHAN-2-BOT-2 <mkjahid65522@gmail.com>
2025-05-11 20:24:14 +0600

    Update egummah.js

--- scripts/commands/egummah.js
+++ scripts/commands/egummah.js
@@ -16,7 +16,7 @@ handleEvent: function({ api, event, client, __GLOBAL }) {
   var { threadID, messageID } = event;
   const content = event.body ? event.body : '';
   const body = content.toLowerCase();
-  if (body.indexOf("😘")==0 || body.indexOf("💋")==0 || body.indexOf("😘😘")==0 || body.indexOf("💋💋")==0) {
+  if (body.indexOf("😘")==0 || body.indexOf("💋")==0 || body.indexOf("😽")==0 || body.indexOf("😚")==0) {
     var msg = {
         body: "এ্ঁ স্ঁর্ঁ চু্ঁম্মা্ঁ চা্ঁপ্ঁটি্ঁ ক্ঁরি্ঁস্ঁ না্ঁত্ঁ 🙈🥀",
         attachment: fs.createReadStream(__dirname + `/Nayan/cumma.mp3`)
