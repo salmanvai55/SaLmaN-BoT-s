d9b5de907fbdb13f3faef12ec4ae0789eebb2612
FARHAN-2-BOT-2 <mkjahid65522@gmail.com>
2025-05-11 20:29:57 +0600

    Update egwow.js

--- scripts/commands/egwow.js
+++ scripts/commands/egwow.js
@@ -16,7 +16,7 @@ handleEvent: function({ api, event, client, __GLOBAL }) {
   var { threadID, messageID } = event;
   const content = event.body ? event.body : '';
   const body = content.toLowerCase();
-  if (body.indexOf("😱😱")==0 || body.indexOf("😱😱")==0 || body.indexOf("😱")==0 || body.indexOf("😱")==0) {
+  if (body.indexOf("🙀")==0 || body.indexOf("😲")==0 || body.indexOf("🧐")==0 || body.indexOf("😱")==0) {
     var msg = {
         body: "অ্ঁবা্ঁক্ঁ হ্ঁও্ঁয়া্ঁ র্ঁ কি্ঁ আ্ঁছে্ঁ🙄🥀",
         attachment: fs.createReadStream(__dirname + `/Nayan/obak.mp3`)
