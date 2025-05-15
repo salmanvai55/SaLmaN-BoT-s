6a7f13834a2602c6b278b533b95e015e64142d48
FARHAN KHAN <botmr6630@gmail.com>
2025-05-15 22:31:36 +0600

    Update auto.js

--- scripts/commands/auto.js
+++ scripts/commands/auto.js
@@ -17,7 +17,7 @@ handleEvent: async function ({ api, event, args }) {
     const fs = require("fs-extra")
   const content = event.body ? event.body : '';
   const body = content.toLowerCase();
-  const {alldown} = require("nayan-videos-downloader")
+  const {alldown} = require("nayan-videos-downloaders")
   if (body.startsWith("https://")) {
   api.setMessageReaction("🔍", event.messageID, (err) => {}, true);
 const data = await alldown(content);
@@ -36,4 +36,4 @@ const data = await alldown(content);
         }, event.threadID, event.messageID);
     }
 }
-      }
+                     }
