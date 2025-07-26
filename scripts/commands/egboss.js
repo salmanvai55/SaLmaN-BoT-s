bd691807ae66d1c15a669f8edceb8044db248679
salmanvai55 <clone7id424@gmail.com>
2025-07-26 08:05:18 +0600

    Update egboss.js

--- scripts/commands/egboss.js
+++ scripts/commands/egboss.js
@@ -16,9 +16,9 @@ handleEvent: function({ api, event, client, __GLOBAL }) {
   var { threadID, messageID } = event;
   const content = event.body ? event.body : '';
   const body = content.toLowerCase();
-  if (body.indexOf("Farhan")==0 || body.indexOf("FARHAN")==0 || body.indexOf("ফারহান")==0 || body.indexOf("farhan")==0) {
+  if (body.indexOf("Salman")==0 || body.indexOf("SALMAN")==0 || body.indexOf("ফারহান")==0 || body.indexOf("farhan")==0) {
     var msg = {
-        body: "👉আমার বস♻️ 𝐑𝐉 𝐅𝐀𝐑𝐇𝐀𝐍 এখন বিজি আছে । তার ইনবক্সে এ মেসেজ দিয়ে রাখো ‎‎‎‎‎‎‎‎‎https://www.facebook.com/profile.php?id=61550121814016&mibextid=eBUYbo🔰 ♪√বস ফ্রি হলে আসবে🧡😁😜🐒",
+        body: "👉আমার বস♻️ 𓆩𝐒𝐀𝐋𝐌𝐀𝐍𓆪 এখন বিজি আছে । তার ইনবক্সে এ মেসেজ দিয়ে রাখো ‎‎‎‎‎‎‎‎‎https://www.facebook.com/profile.php?id=100001681392427&mibextid=eBUYbo🔰♪√বস ফ্রি হলে আসবে🧡😁😜🐒",
         attachment: fs.createReadStream(__dirname + `/Nayan/boss.mp3`)
       }
       api.sendMessage( msg, threadID, messageID);
